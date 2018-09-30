58c1efd742ad27da7e8061ac4e10bedc1ac33079
Mohammed Affan <affan2@gmail.com>
2018-09-30 19:48:41 +0400

    try except to handle other providers

--- allauth/socialaccount/providers/oauth2/urls.py
+++ allauth/socialaccount/providers/oauth2/urls.py
@@ -1,13 +1,23 @@
 from django.conf.urls import patterns, url, include
+from django.utils.module_loading import import_string
 
 
 def default_urlpatterns(provider):
     urlpatterns = patterns(provider.package + '.views',
                            url('^login/$', 'oauth2_login',
                                name=provider.id + "_login"),
-                           url('^logout/$', 'oauth2_logout',
-                               name=provider.id + "_logout"),
                            url('^login/callback/$', 'oauth2_callback',
                                name=provider.id + "_callback"))
+
+    try:
+        logout_view = import_string(provider.package + '.views.oauth2_logout')
+    except ImportError:
+        logout_view = None
+
+    if logout_view is not None:
+        urlpatterns += [
+            url('^logout/$', logout_view,
+                name=provider.id + '_logout'),
+        ]
 
     return patterns('', url('^' + provider.id + '/', include(urlpatterns)))
